d73a7b88aa2a412812a9ad699b27efb0b69ae789
Luis Chee <luis.chee@bairesdev.com>
2022-03-13 00:15:18 -0600

    feat: adding firebase and persistence of store

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,10 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
@@ -24,7 +27,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vuex-persist', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -65,4 +68,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  router: {
+    middleware: 'router-auth',
+  },
 }
---
+++ services/fireinit.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase/app'
+import * as Auth from 'firebase/auth'
+import { getApps } from 'firebase/app'
+import { firebaseConfig } from '~/firebaseConfig'
+
+// eslint-disable-next-line no-unused-expressions
+!getApps().length ? firebase.initializeApp(firebaseConfig) : ''
+
+// export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+export const auth = Auth
+export default firebase
